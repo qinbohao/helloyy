78c8ec44ae15604b5eaa8b0db6d9b114151d9f88
83338848@qq.com <qinbohao>
2016-09-22 10:41:03 +0800

    1.0

---
+++ src/Servlet/GetInfo.java
@@ -0,0 +1,89 @@
+package Servlet;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetInfo
+ */
+@WebServlet("/GetInfo")
+public class GetInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	 public static String getSha1(String str){
+	        if(str==null||str.length()==0){
+	            return null;
+	        }
+	        char hexDigits[] = {'0','1','2','3','4','5','6','7','8','9',
+	                'a','b','c','d','e','f'};
+	        try {
+	            MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+	            mdTemp.update(str.getBytes("UTF-8"));
+
+	            byte[] md = mdTemp.digest();
+	            int j = md.length;
+	            char buf[] = new char[j*2];
+	            int k = 0;
+	            for (int i = 0; i < j; i++) {
+	                byte byte0 = md[i];
+	                buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+	                buf[k++] = hexDigits[byte0 & 0xf];      
+	            }
+	            return new String(buf);
+	        } catch (Exception e) {
+	            // TODO: handle exception
+	            return null;
+	        }
+	    }
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetInfo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=request.getReader();
+		PrintWriter pw=response.getWriter();
+		String line=null;
+		String getreq="";
+		String token="qinbohao";
+		String signature=request.getParameter("signature");
+		String echostr=request.getParameter("echostr");
+		String timestamp=request.getParameter("timestamp");
+		String nonce=request.getParameter("nonce");
+		while((line=br.readLine())!=null){
+			getreq+=line;
+		}
+		if(signature!=null&&echostr!=null){		
+		pw.println(echostr);
+		pw.flush();
+		}else{
+			System.out.println(getreq);
+		pw.print("success");
+		pw.flush();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/sql/java/Info.java
@@ -0,0 +1,33 @@
+package sql.java;
+
+public class Info {
+	private String name;
+	private String cardid;
+	private String schoolid;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCardid() {
+		return cardid;
+	}
+	public void setCardid(String cardid) {
+		this.cardid = cardid;
+	}
+	public String getSchoolid() {
+		return schoolid;
+	}
+	public void setSchoolid(String schoolid) {
+		this.schoolid = schoolid;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
